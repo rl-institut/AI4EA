cc1e15586c702ad00a934fc0cc73114372dfe791
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2025-05-22 15:42:11 +0200

    Resolve templates path

--- webmap/main.py
+++ webmap/main.py
@@ -22,7 +22,7 @@ GEOJSON_FILE = DATA_DIR / "nigeria_admin1.geojson"
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
 app.add_middleware(
     CORSMiddleware,
