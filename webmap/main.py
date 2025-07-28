61769f53f19ea8fa05500d055e42f3ea0edfe4a6
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2025-07-28 13:47:23 +0200

    Add app structure

---
+++ webmap/main.py
@@ -0,0 +1,96 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import HTMLResponse, FileResponse, StreamingResponse
+import numpy as np
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
+from pathlib import Path
+import xarray as xr
+# from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+from uvicorn.middleware.proxy_headers import ProxyHeadersMiddleware
+
+
+BASE_DIR = Path(__file__).resolve().parent
+DATA_DIR = BASE_DIR / "data"
+GEOJSON_FILE = DATA_DIR / "nigeria_admin1.geojson"
+
+# Path to static folder
+static_path = BASE_DIR / "static"
+
+# Load dataset from static folder
+datasets = []
+for country_iso in ("TGO", "NGA", "NER", "BEN", "GHA"):
+    dataset_path = static_path / "data" / f"{country_iso}_timeseries_daily_avg.nc"
+    dataset = xr.open_dataset(dataset_path)
+    datasets.append(dataset)
+
+# Combine all datasets within one larger one
+dataset = xr.concat(datasets, dim="location")
+
+app = FastAPI()
+
+app.add_middleware(ProxyHeadersMiddleware)
+
+app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
+templates = Jinja2Templates(directory=BASE_DIR / "templates")
+
+# Define allowed origins
+origins = [
+]
+
+# Optionally, allow localhost during development
+if os.getenv("ENV") != "prod":
+    origins.append("http://localhost:8000")
+else:
+    origins.append(os.getenv("TRUSTED_HOST"))
+
+print(origins)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_headers=["Authorization", "Content-Type"],
+)
+
+
+
+@app.get("/", response_class=HTMLResponse)
+async def get_map(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/get_data/{selector}")
+async def get_data(selector: str):
+    try:
+        # Slice using provided coordinate and value
+        selected_data = np.round(dataset["value"].sel(location=selector).values / 1e6, 3)
+        # Convert to dict for JSON serialization
+        data_dict = selected_data.tolist()
+
+        return data_dict
+
+    except KeyError:
+        raise HTTPException(status_code=404, detail="Value not found in dataset.")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.get("/imprint", response_class=HTMLResponse)
+async def imprint(request: Request):
+    return templates.TemplateResponse("imprint.html", {"request": request})
+
+@app.get("/privacy", response_class=HTMLResponse)
+async def privacy(request: Request):
+    return templates.TemplateResponse("privacy.html", {"request": request})
+
+@app.get("/geojson/nigeria", response_class=FileResponse)
+async def nigeria_geojson():
+    return FileResponse(GEOJSON_FILE, media_type="application/geo+json")
+
+
+
+
